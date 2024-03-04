8b5056cf68db5850d4aa59e2ee862f0d1a1374f8
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-03-04 10:24:09 +0100

    Added wireframing on loaded mesh

--- mesher-client/src/components/Mesh.tsx
+++ mesher-client/src/components/Mesh.tsx
@@ -1,6 +1,6 @@
 import { useLoader } from '@react-three/fiber';
 import { GUI } from 'dat.gui';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { TextureLoader } from 'three';
 
 interface MeshProps {
@@ -13,24 +13,28 @@ interface MeshProps {
 }
 
 const Mesh = (props: MeshProps) => {
-
+    const [wireframe, setWireframe] = useState<boolean>();
     const mesh = useRef<THREE.Mesh>(null!);
     //TODO: generate textures as well???? for now kept as default. could also load random from a pool of defaults.
-    const colorMap = useLoader(TextureLoader, 'rough-architect-structure-stonewall-superb_1323-42.avif')
-
+    const colorMap = useLoader(TextureLoader, 'rough-architect-structure-stonewall-superb_1323-42.avif');
+    var obj = {Wireframe: function() {setWireframe(!wireframe)}};
     useEffect(() => {
-        const gui = new GUI()
+        const gui = new GUI();
         gui.add(mesh.current.rotation, 'x', 0, Math.PI * 2);
         gui.add(mesh.current.rotation, 'y', 0, Math.PI * 2);
         gui.add(mesh.current.rotation, 'z', 0, Math.PI * 2);
+        gui.add(obj, 'Wireframe');
         return () => {
           gui.destroy()
         }
-      }, [])
+      }, [wireframe])
+      
     return (
     <mesh rotation-x={props.rotationX} rotation-y={props.rotationY} ref={mesh}>
         <boxGeometry args={[props.geometryX, props.geometryY, props.geometryZ]} />
-        <meshPhysicalMaterial metalness={3} roughness={0.36} clearcoat={1} transmission={1} ior={1.53} thickness={5} map={colorMap}/>
+        <meshPhysicalMaterial metalness={3} roughness={0.36} clearcoat={1} 
+        transmission={1} ior={1.53} thickness={5} 
+        map={colorMap} wireframe={wireframe}/>
     </mesh>
     
     )
