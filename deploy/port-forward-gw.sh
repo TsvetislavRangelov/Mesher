ab04515426d7b37a089cab99c8fe3bc5d237504a
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-03-29 19:23:06 +0100

    Set external IP to envoy gw

--- deploy/port-forward-gw.sh
+++ deploy/port-forward-gw.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+# Export the envoy service name into an env variable.export ENVOY_SERVICE=$(kubectl get svc -n envoy-gateway-system --selector=gateway.envoyproxy.io/owning-gateway-namespace=default,gateway.envoyproxy.io/ownin
 
-# Export the envoy service name into an env variable.
-export ENVOY_SERVICE=$(kubectl get svc -n envoy-gateway-system --selector=gateway.envoyproxy.io/owning-gateway-namespace=default,gateway.envoyproxy.io/owning-gateway-name=eg -o jsonpath='{.items[0].metadata.name}')
-
-# Port forward envoy to localhost on port 8888.
-kubectl -n envoy-gateway-system port-forward service/${ENVOY_SERVICE} 8888:80 &
+# Assigns an external ip address so that the gateway can be reachable via the web client.
+kubectl patch gateway eg --type=json --patch '[{
+   "op": "add",
+   "path": "/spec/addresses",
+   "value": [{
+      "type": "IPAddress",
+      "value": "198.51.118.26"
+   }]
+}]'
