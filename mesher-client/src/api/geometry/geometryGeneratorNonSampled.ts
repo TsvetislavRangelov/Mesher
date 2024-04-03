638f4bb7bddae7fe20f0ab9668cf3777893c3ca2
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-04-03 10:00:06 +0200

    tryout loopback address for local k8s

--- mesher-client/src/api/geometry/geometryGeneratorNonSampled.ts
+++ mesher-client/src/api/geometry/geometryGeneratorNonSampled.ts
@@ -2,15 +2,11 @@
 import axios from "axios";
 
 
-const url:string = 'http://envoy-default-eg-e41e7b31.envoy-gateway-system.svc.cluster.local/api/GeometryNonSampled';
+const url:string = 'http://127.0.0.1:80/api/GeometryNonSampled';
 
 export const getGeometryVector = async (): Promise<number[] | undefined> => {
     try{
-        const res =  (await axios.get<number[]>(url + '/GenerateVertices', {
-            headers: {
-                'Host': 'www.mesher.com'
-            }
-        })).data;
+        const res =  (await axios.get<number[]>(url + '/GenerateVertices')).data;
         console.log(res);
         return res;
     }
