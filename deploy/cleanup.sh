7b570ccf02a574863f887f8f7f5070c3b6faef26
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-04-10 09:28:39 +0200

    Configured environments for local development and production along with deployment to the netlab

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -2,6 +2,3 @@
 
 # Cleanup current active deployment.
 kubectl delete -f . --ignore-not-found=true
-
-# Uninstall the gateway.
-helm uninstall eg -n envoy-gateway-system
--- deploy/deploy-local-k8s.sh
+++ deploy/deploy-local-k8s.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# install CRDs and Envoy Gateway
-helm install eg oci://docker.io/envoyproxy/gateway-helm --version v0.0.0-latest -n envoy-gateway-system --create-namespace
-
-# Wait for Envoy Gateway to become available
-kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
 
 kubectl apply -f .
