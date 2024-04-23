7524609844fce786d037661d84d4ef8961c0e82d
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-04-23 16:23:02 +0200

    Add routing via react-router

--- mesher-client/src/main.tsx
+++ mesher-client/src/main.tsx
@@ -3,6 +3,10 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Auth0Provider } from '@auth0/auth0-react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const httpClient = new QueryClient({
   defaultOptions: {
@@ -12,18 +16,25 @@ const httpClient = new QueryClient({
   }
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 const domain: string = `${import.meta.env.VITE_AUTH0_DOMAIN}`;
 const clientId: string = `${import.meta.env.VITE_AUTH0_CLIENT_ID}`;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider 
-  domain={domain} cacheLocation='memory' useRefreshTokens={true} clientId={clientId} authorizationParams={{
+  domain={domain} cacheLocation='localstorage' useRefreshTokens={true} clientId={clientId} authorizationParams={{
     redirect_uri: `${import.meta.env.VITE_AUTH0_REDIRECT_URI}`,
     audience: "https://mesher.eu.auth0.com/api/v2/",
     scope: "read:current_user update:current_user_metadata"
   }}>
   <QueryClientProvider client={httpClient}>
-  <App />
+  <RouterProvider router={router} />
   </QueryClientProvider>
   </Auth0Provider>
 )
