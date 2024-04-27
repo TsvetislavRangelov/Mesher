f0eba43399b291ff4ae51c0796494d866c77ddd3
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-04-27 19:48:03 +0200

    Fix code issues from Codacy 2

--- Services/GeometryGeneratorNonSampled/Program.cs
+++ Services/GeometryGeneratorNonSampled/Program.cs
@@ -1,7 +1,7 @@
 using GeometryGeneratorNonSampled.Services.Implementation;
 using GeometryGeneratorNonSampled.Services.Interfaces;
 
-var allowSpecificOrigins = "dev";
+const string allowSpecificOrigins = "dev";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
