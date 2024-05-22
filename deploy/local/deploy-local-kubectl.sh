f692f10b5014a1c399a332d2b0510b83ce46255c
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-05-22 14:37:21 +0200

    Fix breaking change in 1.0.1 yaml file for envoy gateway

--- deploy/local/deploy-local-kubectl.sh
+++ deploy/local/deploy-local-kubectl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/latest/install.yaml
+kubectl apply --server-side --force-conflicts -f https://github.com/envoyproxy/gateway/releases/download/v1.0.1/install.yaml
 
 kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
 
