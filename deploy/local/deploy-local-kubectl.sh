b671304796b06ecfb1ff95d413c9a9aa481fc959
TsvetislavRangelov <cveti2002mnt@gmail.com>
2024-05-30 19:22:54 +0200

    Enable invariant globalization

--- deploy/local/deploy-local-kubectl.sh
+++ deploy/local/deploy-local-kubectl.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl apply --server-side --force-conflicts -f https://github.com/envoyproxy/gateway/releases/download/v1.0.1/install.yaml
+kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v1.0.1/install.yaml
 
 kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
-
-
-export ENVOY_SERVICE=$(kubectl get svc -n envoy-gateway-system --selector=gateway.envoyproxy.io/owning-gateway-namespace=default,gateway.envoyproxy.io/owning-gateway-name=eg -o jsonpath='{.items[0].metadata.name}')
-
-kubectl -n envoy-gateway-system port-forward service/${ENVOY_SERVICE} 8043:443 &
-
-
 
 kubectl apply -f .
